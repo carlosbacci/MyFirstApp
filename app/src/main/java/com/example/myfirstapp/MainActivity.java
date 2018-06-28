781e87c0e2c8f6301f2cbe6c9b946a544e0f7d8b
Carlos Bacci <carlosabacci@gmail.com>
2018-06-27 23:01:39 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.novolayout);
+    }
+
+    public void tirarFoto(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+                Bitmap img = (Bitmap) bundle.get("data");
+
+                ImageView image = (ImageView) findViewById(R.id.imageViewFoto);
+                image.setImageBitmap(img);
+            }
+        }
+    }
+}
