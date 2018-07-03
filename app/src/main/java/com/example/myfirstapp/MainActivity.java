3572761d669478bbb224007b4e5d144327cb99ac
Carlos Bacci <carlosabacci@gmail.com>
2018-07-02 22:25:56 -0300

    Database Android

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,7 +5,12 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.myfirstapp.dao.MembroDAO;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.novolayout);
+
+        Button continuar = (Button) findViewById(R.id.buttonContinuar);
+        continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MembroDAO dao = new MembroDAO(getBaseContext());
+                EditText editNome = (EditText) findViewById(R.id.editTextNome);
+                EditText editDtNascimento = (EditText) findViewById(R.id.editTextDataNascimento);
+                EditText editEmail = (EditText) findViewById(R.id.editTextEmail);
+                EditText editCelular = (EditText) findViewById(R.id.editTextCelular);
+                String nome = editNome.getText().toString();
+                String dtNascimento = editDtNascimento.getText().toString();
+                String email = editEmail.getText().toString();
+                String celular = editCelular.getText().toString();
+
+                String resultado = dao.insereMembro(nome, dtNascimento, email, celular);
+
+                Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void tirarFoto(View view) {
